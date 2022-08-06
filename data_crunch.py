7a79d4783f470da5e269547d827e557b73e7700d
critmis <44994399+ayambasyal@users.noreply.github.com>
2022-08-06 23:43:03 +0545

    more crunching

--- data_crunch.py
+++ data_crunch.py
@@ -1,9 +1,34 @@
-from ast import main
 import numpy as np
 from PIL import ImageTk, Image, ImageChops, ImageOps , ImageEnhance
 import cv2
 
-im0 = Image.open(r"C:\Users\97798\Documents\TeamOgreHackfest\data\gee_slope.jpg")
+def binarize(img):
+
+  #initialize threshold
+  thresh=50
+
+  #convert image to greyscale
+  img=img.convert('L') 
+
+  width,height=img.size
+
+  #traverse through pixels 
+  for x in range(width):
+    for y in range(height):
+
+      #if intensity less than threshold, assign white
+      if img.getpixel((x,y)) < thresh:
+        img.putpixel((x,y),0)
+
+      #if intensity greater than threshold, assign black 
+      else:
+        img.putpixel((x,y),255)
+
+  return img
+
+
+
+im0 = Image.open(r"\data\gee_slope.jpg")
 im1 = Image.open(r"C:\Users\97798\Documents\TeamOgreHackfest\data\gee_rainfall.jpg")
 im2 = Image.open(r"C:\Users\97798\Documents\TeamOgreHackfest\data\gee_soil.jpg")
 
@@ -17,6 +42,5 @@ im2 = Image.open(r"C:\Users\97798\Documents\TeamOgreHackfest\data\gee_soil.jpg")
 
 im = ImageChops.multiply(im2,im1)
 im = ImageChops.multiply(im,im0)
-
-im.show()
-
+bin_image = binarize(im)
+bin_image.show()
